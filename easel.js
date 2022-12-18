f69707021c4377b59e56114779c0820bf934f214
Farid0558475484 <faridmh@code.edu.az>
2022-12-18 14:37:20 +0400

    object create

--- easel.js
+++ easel.js
@@ -10,7 +10,7 @@
 
 // }
 
-console.log("Request data ...");
+// console.log("Request data ...");
 
 // setTimeout(() => {
 //   console.log("Preparing data ...");
@@ -28,38 +28,90 @@ console.log("Request data ...");
 // }
 // , 2000);
 
-const p = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    console.log("Preparing data ...");
+// const p = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log("Preparing data ...");
 
-    const backendData = {
-      server: "aws",
-      port: 2000,
-      status: "working",
-    };
-    resolve(backendData);
-  }, 2000);
-});
- 
-p.then((data) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      data.modified = true;
-      resolve(data);
-      // reject(data);
-    }, 2000);
-  });
-}
-)
-.then(clientData => {
-  clientData.fromPromise = true;
-  return clientData;
-}
-)
-.then(data => {
-  console.log("Modified", data);
-}
-)
-.catch(err => console.error("Error: ", err))
-.finally(() => console.log("Finally"));
+//     const backendData = {
+//       server: "aws",
+//       port: 2000,
+//       status: "working",
+//     };
+//     resolve(backendData);
+//   }, 2000);
+// });
 
+// p.then((data) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       data.modified = true;
+//       resolve(data);
+//       // reject(data);
+//     }, 2000);
+//   });
+// }
+// )
+// .then(clientData => {
+//   clientData.fromPromise = true;
+//   return clientData;
+// }
+// )
+// .then(data => {
+//   console.log("Modified", data);
+// }
+// )
+// .catch(err => console.error("Error: ", err))
+// .finally(() => console.log("Finally"));
+
+// const sleep = ms => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(), ms)
+//   })
+// }
+
+// sleep(2000).then(() => console.log("After 2 sec"))
+// sleep(3000).then(() => console.log("After 3 sec"))
+
+// Promise.all([sleep(2000), sleep(5000)])
+// .then(() => {
+//   console.log("All promises")
+// }
+// )
+
+const person = Object.create(
+  {
+    calculateAge() {
+      console.log("Age: ", new Date().getFullYear() - this.birthYear);
+    }
+  },
+  {
+    name: {
+      value: "Farid",
+      enumerable: true,
+      writable: true,
+      configurable: true,
+    },
+    birthYear: {
+      value: 1997,
+      enumerable: false,
+    },
+    age: {
+      get() {
+        return calculateAge();
+      },
+      set(value) {
+        console.log("Set age", value);
+
+      },
+    },
+  }
+);
+
+// const person = {
+//   name: "Farid",
+//   birthYear: 1997,
+// }
+
+for (let key in person) {
+  console.log("key", key, person[key]);
+}
